a7fb6c5101b3f4809ac044f5f405b089d92f9836
osamamagar <osamamagar99@gmail.com>
2024-03-14 17:49:59 +0200

    added last touches for posts and autenticate

--- blog/api/views.py
+++ blog/api/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from rest_framework.authtoken.models import Token
 from rest_framework import generics, viewsets
 from django.views.decorators.csrf import csrf_exempt
-from blog.models import User, Post
+from blog.models import User, Post, PasswordResetToken
 from .serializers import UserSerializer, PostSerializer
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -14,11 +14,10 @@ from django.utils.encoding import force_bytes, smart_str
 from django.template.loader import render_to_string
 from django.utils import timezone
 import uuid
-from .serializers import RegisterSerializer, PostSerializer,UserSerializer 
+from .serializers import RegisterSerializer, PostSerializer, UserSerializer
 
 
-
-#-----------------------View for user to login page ----------------
+# -----------------------View for user to login page ----------------
 @api_view(['POST'])
 @permission_classes([])
 def login_view(request):
@@ -31,6 +30,9 @@ def login_view(request):
     user = authenticate(request, username=username, password=password)
 
     if user is not None:
+        if not user.is_email_verified:  # Check if email is not verified
+
+            return Response({"status": "error", "message": "Email not verified. Please check your email to activate your account."}, status=status.HTTP_400_BAD_REQUEST)
         login(request, user)
 
         # Check if a token already exists for the user
@@ -42,10 +44,10 @@ def login_view(request):
             "token": token.key
         }, status=status.HTTP_200_OK)
     else:
-        return Response({"status": "error", "message": "Invalid username or password."}, status=status.HTTP_400_BAD_REQUEST)
-    
-    
-    #-------------------Logs out for current users logged in---------------------
+        return Response({"status": "error", "message": "Invalid username or password "}, status=status.HTTP_400_BAD_REQUEST)
+
+    # -------------------Logs out for current users logged in---------------------
+
 
 @api_view(['POST'])
 def logout_view(request):
@@ -63,7 +65,7 @@ def logout_view(request):
     return Response({"status": "success", "message": "Logout successful."}, status=status.HTTP_200_OK)
 
 
-#-----------------------Update User---------------------
+# -----------------------Update User---------------------
 class UpdateUser(generics.UpdateAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = User.objects.all()
@@ -72,24 +74,26 @@ class UpdateUser(generics.UpdateAPIView):
     def get_object(self):
         return self.request.user
 
-#--------------------Delete User---------------------
+# --------------------Delete User---------------------
+
 
 class DeleteUser(generics.DestroyAPIView):
-    permission_classes = (AllowAny,)  
-    queryset= User.objects.all()
-    serializer_class=UserSerializer
-
-
-    
-#--------------------------Retrieve Current User Profile-----------------------
-class RetrieveUser(generics.RetrieveAPIView):
-    permission_classes = (AllowAny,)  
+    permission_classes = (AllowAny,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-#-----------------------------------Register User------------------------------
+
+# --------------------------Retrieve Current User Profile-----------------------
+class RetrieveUser(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# -----------------------------------Register User------------------------------
+
+
 @api_view(['POST'])
-@permission_classes([AllowAny])
+@permission_classes([AllowAny,])
 def register_user(request):
     serializer = RegisterSerializer(data=request.data)
     if serializer.is_valid():
@@ -113,18 +117,22 @@ def generate_activation_token(user):
     return token
 
 # -------------------send_activation_email-----------------
+
+
 def send_activation_email(user, activation_token, current_site):
     subject = 'Activate Your Account'
     message = render_to_string('blog/activation_email.html', {
         'user': user,
-        'protocol': 'http',  
+        'protocol': 'http',
         'domain': current_site.domain,
         'uid': urlsafe_base64_encode(force_bytes(user.pk)),
         'token': activation_token,
     })
     user.email_user(subject, message)
 
-#
+# -------------------------Activate user-------------------------------------
+
+
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 @csrf_exempt
@@ -132,8 +140,9 @@ def activate_user(request, uidb64, token):
     try:
         uid = smart_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-        password_reset_token = PasswordResetToken.objects.get(user=user, token=token)
-        
+        password_reset_token = PasswordResetToken.objects.get(
+            user=user, token=token)
+
         expiration_time = 24 * 60 * 60  # 24 hours in seconds
 
         if not user.is_email_verified and (timezone.now() - password_reset_token.created_at).total_seconds() < expiration_time:
@@ -145,8 +154,12 @@ def activate_user(request, uidb64, token):
             return Response({'message': 'Invalid activation link.'}, status=status.HTTP_400_BAD_REQUEST)
     except (TypeError, ValueError, OverflowError, User.DoesNotExist, PasswordResetToken.DoesNotExist):
         return Response({'message': 'Invalid activation link.'}, status=status.HTTP_400_BAD_REQUEST)
-    
 
+
+# -------------------------For POST with CRUD operation-----------------------------------------
+
+
+# ---------------Method post-------------------
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def create_post(request):
@@ -155,10 +168,10 @@ def create_post(request):
         post_serializer = PostSerializer(data=request.data)
         if post_serializer.is_valid():
             post_serializer.save()
-            
+
             # Retrieve the token associated with the authenticated user
             user_token = Token.objects.get(user=request.user)
-            
+
             # Include token in the response
             response_data = {
                 "message": "Post published successfully",
@@ -166,7 +179,10 @@ def create_post(request):
             }
             return Response(response_data, status=201)
         return Response(post_serializer.errors, status=400)
-    
+
+
+# ---------------Method get-------------------
+
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def post_list(request):
@@ -174,6 +190,9 @@ def post_list(request):
         posts = Post.objects.all()
         serialized_posts = PostSerializer(posts, many=True).data
         return Response({"data": serialized_posts, "message": "success"}, status=200)
+
+
+# ---------------All Methods-------------------
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated])
@@ -186,7 +205,7 @@ def post_detail(request, id):
     if request.method == 'GET':
         post_serialized = PostSerializer(post).data
         return Response({"Data": post_serialized}, status=200)
-    
+
     elif request.method == 'PUT':
         # Check if the current user is the author of the post
         if request.user == post.author:
@@ -205,10 +224,9 @@ def post_detail(request, id):
             return Response({"message": "The post was deleted", "object_deleted": id}, status=200)
         else:
             return Response({"message": "You are not authorized to delete this post"}, status=403)
-        
 
 
-#--------- if use viewset instead regular function-----------------
+# --------- if use viewset instead regular function-----------------
 
 class PostsView(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('id')
@@ -217,10 +235,9 @@ class PostsView(viewsets.ModelViewSet):
 
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
-    
+
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
-    
+
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
-    
